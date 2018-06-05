f4e50c1810dbe232d7cf2bab0c21f9e15ddffded
Mallory Feuer <mallory.feuer@gmail.com>
2018-06-05 20:08:51 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -16,7 +16,7 @@ class Post < ActiveRecord::Base
   def clickbaity
     if !title
       errors.add(:title, "Post needs a title")
-    elsif self.title.exclude?("Won't Believe" || "Secret" || "Top" || "Guess")
+    elsif title.exclude?("Won't Believe" || "Secret" || "Top" || "Guess")
       errors.add(:title, "not clickbait-y enough")
     end
   end
